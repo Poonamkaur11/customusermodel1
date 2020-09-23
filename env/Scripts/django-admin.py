91530a2baf6cb0afed6c3eaf4566dcf9a337a89a
Poonamkaur11 <poonamkaur1108@gmail.com>
2020-09-23 18:56:02 +0530

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\data\django-custom-user-model\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ users/forms.py
@@ -0,0 +1,19 @@
+import uuid
+
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
